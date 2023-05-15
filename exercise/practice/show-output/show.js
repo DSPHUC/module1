72fca0d11e1c5532708edf16914f8b14ac50f270
DSPHUC <phuc.dongsi@gmail.com>
2023-05-15 15:18:18 +0700

    create file show output

---
+++ exercise/practice/show-output/show.js
@@ -0,0 +1,12 @@
+function showMessage(){
+    alert('Konichiwa');
+}
+function showMessage2(){
+    document.getElementById('messageDiv2').innerHTML = 'Xin chào!';
+}
+function showMessage3(){
+    document.write('Hello')
+}
+function showMessage4(){
+    console.log('Ni hảo!');
+}
