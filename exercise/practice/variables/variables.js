4d9dce9a4e504a5592c90b0698aa12ab1c20d17b
DSPHUC <phuc.dongsi@gmail.com>
2023-05-16 14:13:34 +0700

    create variables

---
+++ exercise/practice/variables/variables.js
@@ -0,0 +1,29 @@
+// Bài 1
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+// //Bài 2
+let width = 20
+let height = 10
+let area = width * height
+document.write('<br/>')
+document.write('Area = ' + area)
+// Bài 3
+a = prompt("Nhập a:");
+b = prompt("Nhập b:");
+
+if (b % a == 0) {
+    alert("a là bội số của b");
+}
+else { 
+    alert("a không phải là bội số của b"); 
+}
