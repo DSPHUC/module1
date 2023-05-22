cfb88028d2073a8718ed420042f57fcee2779839
DSPHUC <phuc.dongsi@gmail.com>
2023-05-22 16:13:27 +0700

    hang dang that dang nho

---
+++ exercise/excercise/excercise-home/constant-equality/constant.js
@@ -0,0 +1,16 @@
+let a=10;
+let b=15;
+document.write(`(${a}+${b})^2=${a}^2+2*${a}*${b}+${b}^2`);
+document.write('<br/>')
+document.write(`(${a}-${b})^2=${a}^2-2*${a}*${b}+${b}^2`);
+document.write('<br/>')
+document.write(`${a}^2-${b}^2=(${a}-${b})*(${a}+${b})`);
+document.write('<br/>')
+document.write(`(${a}+${b})^3=${a}^3+3*${a}^2*${b}+3${a}*${b}^2+${b}^3`);
+document.write('<br/>')
+document.write(`(${a}-${b})^3=${a}^3-3*${a}^2*${b}+3${a}*${b}^2-${b}^3`);
+document.write('<br/>')
+document.write(`${a}^3+${b}^3=(${a}^3+${b}^3)*(${a}^2-${a}*${b}+${b})`);
+document.write('<br/>')
+document.write(`${a}^3-${b}^3=(${a}-${b})*(${a}^2+${a}*${b}+${b}^2)`);
+    
