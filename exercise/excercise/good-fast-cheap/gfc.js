8c9afa92075189d032216c5136c99b6b58891044
DSPHUC <phuc.dongsi@gmail.com>
2023-05-23 22:19:41 +0700

    udapte filfe GFC

--- exercise/excercise/good-fast-cheap/gfc.js
+++ exercise/excercise/good-fast-cheap/gfc.js
@@ -1,8 +1,32 @@
-let check1 = document.getElementById('good');
-let check2 = document.getElementById('fast');
-let check3 = document.getElementById('cheap');
-if (condition) {
-    
-} else {
-    
+function checkClick(id) {
+    let checkG = document.getElementById("good");
+    let checkF = document.getElementById("fast");
+    let checkC = document.getElementById("cheap");
+    // // if (document.querySelectorAll("input.check1"))
+    // let checked1 =document.querySelectorAll("check1").length ==true;
+    // let checked2 =document.querySelectorAll("check2").length ==true;
+    // let checked3 =document.querySelectorAll("check3").length ==true;
+    // if  (checked1.checked==true &&checked2.checked==true){
+    //     checked1.checked==false;
+    // }
+    // else if  (checked3.checked==true &&checked2.checked==true){
+    //     checked2.checked==false;
+    // }
+    // else if  (checked1.checked==true &&checked3.checked==true){
+    //     checked3.checked==false;
+    // }
+    switch (id) {
+        case "good":
+            if (checkedG.checked == true && checkedF.checked == true) {
+                checkedG.checked == false;
+            }
+        case "fast":
+            if (checked3.checked == true && checkedF.checked == true) {
+                checkedF.checked == false;
+            }
+        case "cheap":
+            if (checkedG.checked == true && checkedC.checked == true) {
+                checkedC.checked == false;
+            }
+    }
 }
