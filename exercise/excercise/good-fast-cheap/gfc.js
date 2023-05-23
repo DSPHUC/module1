f690dcd8fa6d0a07336104fed6d111033715cd99
DSPHUC <phuc.dongsi@gmail.com>
2023-05-23 20:20:11 +0700

    create GFC

---
+++ exercise/excercise/good-fast-cheap/gfc.js
@@ -0,0 +1,8 @@
+let check1 = document.getElementById('good');
+let check2 = document.getElementById('fast');
+let check3 = document.getElementById('cheap');
+if (condition) {
+    
+} else {
+    
+}
