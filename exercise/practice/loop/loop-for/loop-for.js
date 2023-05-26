8ed3cafdd8ecd5812b554cc9aa16bb4de1c5e978
DSPHUC <phuc.dongsi@gmail.com>
2023-05-26 07:57:26 +0700

    create practice loop

---
+++ exercise/practice/loop/loop-for/loop-for.js
@@ -0,0 +1,12 @@
+//bài1
+// for (let i = 0; i <= 5; i++) {
+//     document.write("The number is N " + i + "<br>");
+// }
+//bài2
+let n = prompt("Nhập");
+let sum = 0;
+for (let i = 0; i <= n; i++) {
+    sum += i;  
+    console.log(sum);
+ }
+    document.write(sum);
---
+++ exercise/practice/loop/loop-while/loop-while.js
@@ -0,0 +1,16 @@
+// bài1
+// let i = parseInt(prompt("Nhập"));
+// let sum = 0;
+// while (i != -1) {
+//     i=parseInt(prompt(""));
+//     alert(i);    
+//     sum += i;
+
+// }
+// alert(sum);
+//bài2
+let i = 1;
+while (i <= 100) {
+    document.write("<hr width =" + i + "%>");
+    i++;
+}
