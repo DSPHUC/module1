de1bb0ae2670836f1eaa6d0612acec79eb20388b
DSPHUC <phuc.dongsi@gmail.com>
2023-05-26 08:11:11 +0700

    create excercise multiplication-table

---
+++ exercise/excercise/multiplication-table/multiplication.js
@@ -0,0 +1,12 @@
+let multi;
+multi = "<table border='1' width='300' cellspacing='0' cellpadding='3'>"
+for (let x = 2; x <= 10; x++) {
+    multi = multi + ("<tr>")
+    for (y = 1; y <= 10; y++) {
+        multi = multi + ("<td>") +x+"x"+y+"="+ x * y + ("</td>");
+
+    }
+    multi = multi + ("</tr>")
+}
+multi = multi + ("<table>");
+document.write(multi);
